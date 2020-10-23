731c5442f566047a1325340b28d8d00544a5c2d6
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-10-23 20:00:30 +0100

    Update Docker configuration
    Files have been moved into their respective directories so they can be mounted better within the Docker container.
    
    Environment variables for pip commands have been moved to environment variable arguments in the docker run command.
    
    The method the current commit ref is fetched has been adjusted as the .git directory is not mounted within the container, instead just the master ref file is mounted in the container and that is now read from.

--- start.bash
+++ start.bash
@@ -6,15 +6,20 @@
 	--name conspiracyai \
 	--hostname conspiracyai \
 	--tmpfs /tmp:rw,noexec,nosuid,nodev \
-	--mount type=bind,source=/srv/conspiracy-ai,destination=/usr/local/src/conspiracyai \
+	--mount type=bind,source=/srv/conspiracy-ai/.git/refs/heads/master,destination=/tmp/conspiracyai/ref.txt,readonly \
+	--mount type=bind,source=/srv/conspiracy-ai/source,destination=/usr/local/src/conspiracyai,readonly \
+	--mount type=bind,source=/srv/conspiracy-ai/config,destination=/usr/local/etc/conspiracyai,readonly \
 	--mount type=bind,source=/srv/conspiracy-ai/cache,destination=/var/tmp/conspiracyai \
+	--mount type=bind,source=/srv/conspiracy-ai/data,destination=/var/lib/conspiracyai \
 	--workdir /usr/local/src/conspiracyai \
+	--env XDG_CACHE_HOME=/var/tmp/conspiracyai \
+	--env PYTHONPYCACHEPREFIX=/var/tmp/conspiracyai/pycache \
 	--network host \
 	--interactive \
 	--tty \
 	--detach \
 	--restart unless-stopped \
 	python:latest \
-	/bin/bash -c 'XDG_CACHE_HOME=/var/tmp/conspiracyai pip install --upgrade --no-color --no-input pip wheel setuptools && \
-		XDG_CACHE_HOME=/var/tmp/conspiracyai pip install --upgrade --no-color --no-input discord.py requests hurry.filesize mysql-connector-python pytz dotmap beautifulsoup4 youtube_dl && \
+	/bin/bash -c 'pip install --upgrade --no-color --no-input pip wheel setuptools && \
+		pip install --upgrade --no-color --no-input discord.py requests hurry.filesize mysql-connector-python pytz dotmap beautifulsoup4 youtube_dl && \
 		python client.py'
