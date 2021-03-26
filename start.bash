9c1adcd802aa4cade2656f5aeb59fa149d3c5a94
viral32111 <19510403+viral32111@users.noreply.github.com>
2021-03-26 15:15:42 +0000

    Update startup script
    Moved dependencies back into it.
    
    Added a 'dev mode' to make things easier for me.

--- start.bash
+++ start.bash
@@ -2,22 +2,81 @@
 
 set -e
 
-docker run \
-	--name conspiracy-ai \
-	--tmpfs /tmp:noexec,nosuid,nodev,size=4G \
-	--mount type=bind,source=/srv/conspiracy-ai/source,target=/srv/conspiracy-ai/source,readonly \
-	--mount type=bind,source=/srv/conspiracy-ai/config,target=/srv/conspiracy-ai/config,readonly \
-	--mount type=bind,source=/srv/conspiracy-ai/modules.txt,target=/srv/conspiracy-ai/modules.txt,readonly \
-	--mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-	--workdir /srv/conspiracy-ai \
-	--env LOCAL_COMMIT_REFERENCE=$(cat /srv/conspiracy-ai/.git/refs/heads/master) \
-	--network host \
-	--tty \
-	--interactive \
-	--restart unless-stopped \
-	--detach \
-	--pull always \
-	python:latest \
-	bash -c 'pip install --requirement modules.txt && python source/client.py'
+if [ "$1" = "dev" ]; then
+	docker run \
+		--name conspiracy-ai \
+		--tmpfs /tmp:noexec,nosuid,nodev,size=4G \
+		--mount type=bind,source=/srv/conspiracy-ai/source,target=/srv/conspiracy-ai/source,readonly \
+		--mount type=bind,source=/srv/conspiracy-ai/config,target=/srv/conspiracy-ai/config,readonly \
+		--mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+		--mount type=bind,source=/home/viral32111/slashcommands,target=/slashcommands \
+		--workdir /srv/conspiracy-ai \
+		--env LOCAL_COMMIT_REFERENCE=$(cat /srv/conspiracy-ai/.git/refs/heads/master) \
+		--network host \
+		--tty \
+		--interactive \
+		--rm \
+		--pull always \
+		python:latest \
+		bash -c 'pip install \
+			git+https://github.com/Rapptz/discord.py \
+			git+https://github.com/ytdl-org/youtube-dl \
+			git+https://github.com/psf/requests \
+			git+https://github.com/drgrib/dotmap \
+			git+https://github.com/mysql/mysql-connector-python \
+			git+https://github.com/Dinnerbone/mcstatus \
+			git+https://github.com/msabramo/requests-unixsocket \
+			python-dateutil \
+			hurry.filesize \
+			pytz \
+			beautifulsoup4 \
+			emoji && \
+			pip install --editable /slashcommands && \
+			bash'
+else
+	docker run \
+		--name conspiracy-ai \
+		--tmpfs /tmp:noexec,nosuid,nodev,size=4G \
+		--mount type=bind,source=/srv/conspiracy-ai/source,target=/srv/conspiracy-ai/source,readonly \
+		--mount type=bind,source=/srv/conspiracy-ai/config,target=/srv/conspiracy-ai/config,readonly \
+		--mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+		--workdir /srv/conspiracy-ai \
+		--env LOCAL_COMMIT_REFERENCE=$(cat /srv/conspiracy-ai/.git/refs/heads/master) \
+		--network host \
+		--tty \
+		--interactive \
+		--restart unless-stopped \
+		--detach \
+		--pull always \
+		python:latest \
+		bash -c 'pip install \
+			git+https://github.com/Rapptz/discord.py \
+			git+https://github.com/ytdl-org/youtube-dl \
+			git+https://github.com/psf/requests \
+			git+https://github.com/drgrib/dotmap \
+			git+https://github.com/mysql/mysql-connector-python \
+			git+https://github.com/Dinnerbone/mcstatus \
+			git+https://github.com/msabramo/requests-unixsocket \
+			git+https://github.com/viral32111/slashcommands \
+			python-dateutil \
+			hurry.filesize \
+			pytz \
+			beautifulsoup4 \
+			emoji && \
+			python source/client.py'
 
-docker attach conspiracy-ai
+	docker attach conspiracy-ai
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
