c843c8bea2ee605a1a47019cd998f35c0d143471
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-06-21 14:34:08 +0100

    Update example code to test workflows

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,6 @@
-console.log( "Hello World!" )
+const printMessage = ( message: string ) => {
+	console.log( "Your message is: " + message )
+}
+
+printMessage( "Hello World" )
+printMessage( "This is an example." )
