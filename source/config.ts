c5440534bd3f265c2b807a364c64c596340b41f3
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-06-24 17:35:23 +0100

    Comment API and config file

--- source/config.ts
+++ source/config.ts
@@ -1,7 +1,11 @@
+// The name and version of this application
 export const APPLICATION_NAME = "viral32111's discord bot"
-export const APPLICATION_VERSION = "0.5.0"
+export const APPLICATION_VERSION = "0.5.0" // Should match package.json
 
+// The contact information for this application
 export const CONTACT_WEBSITE = "https://viral32111.com/contact"
 export const CONTACT_EMAIL = "contact@viral32111.com"
 
+// The base URL to the Discord API
+// https://discord.com/developers/docs/reference#api-versioning
 export const DISCORD_API_URL = "discord.com/api/v10"
--- source/discord/api.ts
+++ source/discord/api.ts
@@ -1,34 +1,54 @@
+// Import from native modules
 import { format } from "util";
-import { request as sendHttpsRequest } from "https";
+import { request as createHttpsRequest } from "https";
 
+// Import from my scripts
 import { APPLICATION_NAME, APPLICATION_VERSION, CONTACT_WEBSITE, CONTACT_EMAIL, DISCORD_API_URL } from "../config.js";
 
+// Sends a request to the Discord API and returns a JSON object
 export function request( endpoint: string, method: string = "GET" ): Promise<any> {
-	const request = sendHttpsRequest( format( "https://%s/%s", DISCORD_API_URL, endpoint ), {
+
+	// Create a HTTP request to the specified endpoint using the specified method
+	const request = createHttpsRequest( format( "https://%s/%s", DISCORD_API_URL, endpoint ), {
 		method: method,
 		headers: {
-			"Accept": "application/json",
-			"Authorization": format( "Bot %s", process.env[ "BOT_TOKEN" ] ),
+			"Accept": "application/json", // Expect a JSON response
+			"Authorization": format( "Bot %s", process.env[ "BOT_TOKEN" ] ), // Authenticate as the bot
 			"User-Agent": format( "%s/%s (%s; %s)", APPLICATION_NAME, APPLICATION_VERSION, CONTACT_WEBSITE, CONTACT_EMAIL )
 		}
 	} )
 
+	// Return a promise that resolves when a valid response is received
 	return new Promise( ( resolve, reject ) => {
+		
+		// When a response is received...
 		request.once( "response", ( response ) => {
+
+			// Set the encoding so text is provided in the data event
 			response.setEncoding( "utf8" )
 
+			// Create and populate an array of strings containing the response body
 			const chunks: string[] = []
 			response.on( "data", ( chunk ) => chunks.push( chunk ) )
 
+			// When all of the response body has been received...
 			response.once( "end", () => {
+
+				// Reject the promise if the response content is not JSON
 				if ( !response.headers[ "content-type" ]?.startsWith( "application/json" ) ) reject( Error( "Did not receive a JSON response" ) )
 
+				// Resolve the promise with the parsed JSON object
 				resolve( JSON.parse( chunks.join( "" ) ) )
+
 			} )
+
 		} )
 
+		// Reject the promise if an error occurs
 		request.once( "error", reject )
 	
+		// Send the HTTP request
 		request.end()
+
 	} )
 }
